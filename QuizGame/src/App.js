08e9c28b6cc9491be9a38021dacfa4f279c4964f
Hajar Mekamy <58553071+hajarmekamy@users.noreply.github.com>
2022-02-14 12:52:10 +0100

    data shuffled function

--- QuizGame/src/App.js
+++ QuizGame/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import StartGame from "./StartGame";
-import Quiz from "./Quiz";
+import StartGame from "./Containers/StartGame";
+import Quiz from "./Containers/Quiz";
+import { data } from "./Assets/QuizData";
 
 function App() {
+  let dataShuffled = data.sort(() => Math.random() - 0.5);
+
   return (
     <Router>
       <div className="App">
@@ -13,7 +16,7 @@ function App() {
         </Routes>
 
         <Routes>
-          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/quiz" element={<Quiz data={dataShuffled} />} />
         </Routes>
       </div>
     </Router>
